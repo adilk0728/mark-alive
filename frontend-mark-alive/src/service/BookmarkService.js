2325b936fccd245acab5ad040d293ec101cae8f0
adilk0728 <adithyaopeth@gmail.com>
2024-02-14 12:13:52 +0000

    Delete VUE

--- frontend-mark-alive/src/service/BookmarkService.js
+++
@@ -1,7 +0,0 @@
-import axios, { Axios } from "axios";
-
-const baseUrl = "http://localhost:8080";
-
-export function getBookmarks() {
-  return axios.get(baseUrl + "/bookmarks").then((res) => res.data);
-}
