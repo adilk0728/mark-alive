8cfd973779ec15eecc92bc93827f6b69614e1fbd
adilk0728 <adithyaopeth@gmail.com>
2024-02-21 17:05:47 +0000

    Add spring actuator

--- src/main/java/com/ad/markalive/model/Bookmark.java
+++ src/main/java/com/ad/markalive/model/Bookmark.java
@@ -1,15 +1,29 @@
 package com.ad.markalive.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+
+import java.time.LocalDate;
 
 public class Bookmark {
     @Id
     private int id;
 
+    @Column("URL")
     private String url;
+    @Column("CREATED")
+    private LocalDate createdOn;
 
+    @Column("REMIND_AFTER_DAY")
+    private int remindAfter;
     public Bookmark(String url) {
         this.url = url;
+    }
+
+    public Bookmark(String url, LocalDate createdOn, int remindAfter){
+        this(url);
+        this.createdOn = createdOn;
+        this.remindAfter = remindAfter;
     }
 
     public int getId() {
@@ -26,5 +40,21 @@ public class Bookmark {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public LocalDate getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDate createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public int getRemindAfter() {
+        return remindAfter;
+    }
+
+    public void setRemindAfter(int remindAfter) {
+        this.remindAfter = remindAfter;
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,9 @@
+DROP TABLE IF EXISTS Bookmark;
+
 CREATE TABLE IF NOT EXISTS Bookmark (
     id int NOT NULL AUTO_INCREMENT,
     url varchar(255) NOT NULL,
+    created date,
+    remind_after_day int,
     PRIMARY KEY(id)
 );
